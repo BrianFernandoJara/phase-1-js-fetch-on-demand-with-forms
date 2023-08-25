78e0c6ca440859422f1bd78f28576011ad6ab1db
BrianFernandoJara <brianfernandojara@gmail.com>
2023-08-25 12:08:10 -0400

    fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  const text = inputForm.querySelector("#searchByID")
+
+    inputForm.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        
+        fetch(`http://localhost:3000/movies/${text.value}`)
+        .then(resp => resp.json())
+        .then(data =>{
+            const title = document.querySelector("#movieDetails h4");
+            const summary = document.querySelector("#movieDetails p");
+
+            title.innerHTML = data.title;
+            summary.innerHTML = data.summary;
+        })
+
+        text.value = "";
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
